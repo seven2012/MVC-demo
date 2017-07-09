547bb0fc3c92a11f85997283e58439eab0a84a43
july77 <ljlpsy11@gmail.com>
2017-07-09 21:45:33 +0800

    run

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+var path = require('path');
+var HtmlwebpackPlugin = require('html-webpack-plugin');
+var OpenBrowserPlugin = require('open-browser-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        port: 9000
+    },
+    plugins: [
+        // new HtmlwebpackPlugin({
+        //     title: 'Webpack-demos',
+        //     filename: 'index.html'
+        // }),
+        new OpenBrowserPlugin({
+            url: 'http://localhost:9000'
+        })
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                loaders: [
+                    'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+                    'image-webpack-loader'
+                ]
+            }
+        ]
+    }
+
+}
